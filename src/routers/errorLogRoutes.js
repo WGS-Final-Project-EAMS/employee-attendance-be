df7561a7f7c835ad35b785ac0d634223a37cdc40
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-08-26 10:56:48 +0700

    :loud_sound: Integrate error log for attendance controller

--- src/routers/errorLogRoutes.js
+++ src/routers/errorLogRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authenticateRole } = require('../middlewares/authMiddleware');
+const { authenticateRole } = require('../middleware/authMiddleware');
 const {
     createErrorLog,
     getErrorLogs,
@@ -11,7 +11,7 @@ const {
 router.post('/error-logs', createErrorLog);
 
 // Route to get all error logs
-router.get('/error-logs', authenticateRole('super_admin'), getErrorLogs);
+router.get('/api/error-logs', authenticateRole('super_admin'), getErrorLogs);
 
 // Route to get a specific error log by ID
 router.get('/error-logs/:id', authenticateRole('super_admin'), getErrorLogById);
