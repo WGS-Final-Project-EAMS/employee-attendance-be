defc4e81d192abe1d9f3b10273592bc7303ca301
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-09-04 13:27:41 +0700

    :hammer: Update nodemailer transport configuration

--- src/utils/emailTransporter.js
+++ src/utils/emailTransporter.js
@@ -2,7 +2,8 @@ const nodemailer = require('nodemailer');
 
 exports.transport = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
+    port: process.env.MAIL_PORT || 587,
+    secure: false,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASSWORD
