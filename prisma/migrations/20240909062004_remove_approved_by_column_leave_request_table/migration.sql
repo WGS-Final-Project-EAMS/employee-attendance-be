d1d22fc87ba8ebdafa8baa32ddd399c3ef84e079
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-09-09 14:11:26 +0700

    :card_file_box: Modify leave request table

---
+++ prisma/migrations/20240909062004_remove_approved_by_column_leave_request_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `approved_by` on the `LeaveRequest` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "LeaveRequest" DROP COLUMN "approved_by";
