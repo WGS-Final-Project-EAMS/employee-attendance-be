0f2b99940630c098b3f5c36d3b7671dc5b3fd1c5
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-09-24 09:01:55 +0700

    :hammer: Create endpoint get employee by user id

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -224,6 +224,37 @@ exports.getEmployeeById = async (req, res) => {
     }
 };
 
+// Get an employee by user id
+exports.getEmployeeByUserId = async (req, res) => {
+    const { user_id } = req.params;
+
+    try {
+        const employee = await prisma.employee.findUnique({
+            where: { user_id },
+            include: {
+                user: true,
+                manager: true,
+            },
+        });
+
+        if (!employee) {
+            return res.status(404).json({ error: "Employee not found" });
+        }
+
+        res.status(200).json(employee);
+    } catch (error) {
+        const { user_id } = req.user;
+
+        await errorLogs({
+            error_message: error.message,
+            error_type: 'GetEmployeeByIdError',
+            user_id,
+        });
+
+        res.status(500).json({ error: error.message });
+    }
+};
+
 // Update an employee
 exports.updateEmployee = async (req, res) => {
     // Input error handling
--- src/routers/employeeRoutes.js
+++ src/routers/employeeRoutes.js
@@ -9,13 +9,16 @@ const { upload } = require('../middleware/multerMiddleware');
 router.post('/employees', authenticateRole(['admin']), upload.single('profile_picture_url'), employeeFormValidation(), employeeController.createEmployee);
 
 // Get all employees
-router.get('/employees', authenticateRole(['admin']), employeeController.getAllEmployees);
+router.get('/employees', authenticateRole(['super_admin', 'admin']), employeeController.getAllEmployees);
 
 // Get inactive employees
 router.get('/employees/inactive', authenticateRole(['admin']), employeeController.getInactiveEmployees);
 
 // Get an employee by ID
 router.get('/employees/:employee_id', authenticateRole(['admin']), employeeController.getEmployeeById);
+
+// Get an employee by user id
+router.get('/employees-user/:user_id', authenticateRole(['super_admin', 'admin']), employeeController.getEmployeeByUserId);
 
 // Update an employee
 router.put('/employees/:user_id', authenticateRole(['admin']), upload.single('profile_picture_url'), employeeFormValidation(), employeeController.updateEmployee);
